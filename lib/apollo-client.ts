a9b10109cb886add287a7c78fa81741b53d4ea3a
EdensWood <woodsleafy@gmail.com>
2025-04-02 20:43:29 +0300

    debugging front end

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,25 +1,17 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { setContext } from "@apollo/client/link/context";
-import Cookies from "js-cookie";
+// Removed Cookies import as it's not needed for session-based auth
 
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_API_URL + "/graphql", // Updated to use env variable
-  credentials: "include",
+  uri: process.env.NEXT_PUBLIC_API_URL + "/graphql",
+  credentials: "include", // Ensure cookies are sent with requests
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = Cookies.get("token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
+// Removed authLink as it's not needed for session-based auth
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: httpLink, // Directly use httpLink
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
