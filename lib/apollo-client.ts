debe9ba30891cd96ea6b1395514f98367474ce93
EdensWood <woodsleafy@gmail.com>
2025-04-06 00:09:00 +0300

    debugging the fixing its run

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,11 +1,10 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { setContext } from "@apollo/client/link/context";
-import { API_BASE_URL } from "./config";
 // Removed Cookies import as it's not needed for session-based auth
 
 const httpLink = createHttpLink({
-  uri: `${API_BASE_URL}/graphql`,
+  uri: `https://backend-l9gz.onrender.com/graphql`,
   credentials: "include",
   headers: {
     // Add this to ensure cookies are sent cross-domain
